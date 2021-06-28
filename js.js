f6b3a1e69f1595b6e5f1b27ce9fd4e0da26afab0
RaginiGandharv <68639669+RaginiGandharv@users.noreply.github.com>
2021-06-28 12:59:53 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:false,
+        navigation:true,
+        navigationText:["",""],
+        autoPlay:true
+    });
+});
