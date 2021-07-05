67a8b14f168c13582518c2bd098c315289ecfbee
tmaji <86413532+tmaji@users.noreply.github.com>
2021-07-05 16:55:13 +0530

    Add files via upload

---
+++ partials/_dbconnect.php
@@ -0,0 +1,12 @@
+<?php
+// Script to connect to the database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "staticwebsite";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+// if($conn){
+//     echo "success";
+// }
+?>
